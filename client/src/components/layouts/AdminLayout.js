04a1f51c5d3191a76a55aca4c97330b66dfed4f5
Alpha72142 <sammurari17@gmail.com>
2025-06-11 22:28:23 +0530

    sidebar and navabar and admin layout has been added

--- client/src/components/layouts/AdminLayout.js
+++ client/src/components/layouts/AdminLayout.js
@@ -1,19 +1,22 @@
+import Sidebar from "@/components/common/Sidebar";
+import Navbar from "@/components/common/Navbar";
+
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Navbar */}
-      <header className="h-12 bg-gray-800 text-white flex items-center px-4">
-        Navbar
+      <header className="h-auto bg-gray-800 text-white flex items-center">
+        <Navbar />
       </header>
 
-      {/* Main area */}
+      {/* Main area: Sidebar + Content */}
       <div className="flex flex-1 overflow-hidden">
-        {/* Sidebar */}
+        {/* Sidebar (left) */}
         <aside className="hidden md:block w-64 bg-gray-200 p-4 overflow-auto">
-          Sidebar
+          <Sidebar />
         </aside>
 
-        {/* Content */}
+        {/* Main content */}
         <main className="flex-1 p-4 overflow-auto">{children}</main>
       </div>
     </div>
---
+++ client/src/redux/sidebarSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  open: true, // Default open on desktop
+};
+
+export const sidebarSlice = createSlice({
+  name: "sidebar",
+  initialState,
+  reducers: {
+    toggleSidebar: (state) => {
+      state.open = !state.open;
+    },
+    openSidebar: (state) => {
+      state.open = true;
+    },
+    closeSidebar: (state) => {
+      state.open = false;
+    },
+  },
+});
+
+export const { toggleSidebar, openSidebar, closeSidebar } =
+  sidebarSlice.actions;
+export default sidebarSlice.reducer;
