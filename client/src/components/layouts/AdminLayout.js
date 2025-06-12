d8c89b7da3dc1370a97ce4bf80c2029bced51d04
Alpha72142 <sammurari17@gmail.com>
2025-06-12 16:26:01 +0530

    sidebar fixed admin layout completed

--- client/src/components/layouts/AdminLayout.js
+++ client/src/components/layouts/AdminLayout.js
@@ -3,21 +3,19 @@ import Navbar from "@/components/common/Navbar";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-amber-100">
       {/* Navbar */}
       <header className="h-auto bg-gray-800 text-white flex items-center">
         <Navbar />
       </header>
 
       {/* Main area: Sidebar + Content */}
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex flex-1 overflow-hidden max-h-[calc(100vh_-_calc(var(--spacing)_*_16))]">
         {/* Sidebar (left) */}
-        <aside className="hidden md:block w-64 bg-gray-200 overflow-auto">
-          <Sidebar />
-        </aside>
+        <Sidebar />
 
         {/* Main content */}
-        <main className="flex-1 p-4 overflow-auto">{children}</main>
+        <main className="flex-1 p-4 overflow-auto">{children}Admin</main>
       </div>
     </div>
   );
